17348c86e14c1b59840c1a76ab234dedbc93b9d6
beyzahyt <beyzardm502@gmail.com>
2021-06-20 17:28:35 +0300

    added report buton

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.IO;
 using System.Web.Mvc;
 using yazilimYapimi.Models.Entity;
-using ChoETL;
+
 
 namespace yazilimYapimi.Controllers
 {
@@ -16,48 +16,15 @@ namespace yazilimYapimi.Controllers
 
         public ActionResult Report()
         {
-            int userId=(int)Session["UserID"];
-            var model = db.tableOrder.Where(o => o.CustomerID == userId).ToList();
-            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\" + userId}.csv";
-            try
-            {
-         
-                using (var w = new ChoCSVWriter<tableOrder>(path).WithFirstLineHeader())
-                {
-                    w.Write(model);
-                    w.Dispose();
-                }
+            var model = db.tableOrder.ToList();
+            return View (model);
+        }
 
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            try
-            {
-                byte[] byteArray = FileToByteArray(path);
-                return new FileContentResult(byteArray, "application/octet-stream");
-            }
-            catch (Exception e)
-            {
-                return View(model);
-            }
-            //return View(model);
-        }
-        public byte[] FileToByteArray(string fileName)
+        public ActionResult GeneratePdf()
         {
-            byte[] buff = null;
-            FileStream fs = new FileStream(fileName,
-                FileMode.Open,
-                FileAccess.Read);
-            BinaryReader br = new BinaryReader(fs);
-            long numBytes = new FileInfo(fileName).Length;
-            buff = br.ReadBytes((int)numBytes);
-            br.Close();
-            fs.Close();
-            return buff;
+            return new Rotativa.ActionAsPdf("Report");
         }
+        
 
     }
 }
